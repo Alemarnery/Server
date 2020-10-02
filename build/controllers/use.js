88b59c9d4337b8d8f12bfcea3c7c6b8434ba7ee5
alemarnery <aleemar.95@gmail.com>
2020-10-01 21:38:53 -0800

    decorator

---
+++ build/controllers/use.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.use = void 0;
+require("reflect-metadata");
+var MetadataKeys_1 = require("./MetadataKeys");
+function use(middleware) {
+    return function (target, key, desc) {
+        var middlewares = Reflect.getMetadata(MetadataKeys_1.MetadataKeys.middleware, target, key) || [];
+    };
+}
+exports.use = use;
